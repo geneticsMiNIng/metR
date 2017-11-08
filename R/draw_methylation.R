d0b2d25c6030a0e863a1024fb7c6429dde7ad606
abrodecka <aleksandrabrodecka@gmail.com>
2017-11-08 18:58:27 +0100

    poprawki draw.methylation

--- R/draw_methylation.R
+++ R/draw_methylation.R
@@ -8,6 +8,8 @@
 #' @param chr chromosom name of region that are being plotted
 #' @param start minimum position of region that are being plotted
 #' @param end maximum position of region that are being plotted
+#' @param bind.probes if TRUE methylation rates on the same position will be binded by vertical lines
+#' @param smooth.methylation if TRUE methylation rates in two probes will be smoothed
 #' @return ggplot object with visualization of regions
 #' @export
 #' @examples
@@ -16,12 +18,19 @@
 #' data <- preprocessing(sample.1, sample.2)
 #' draw.methylation(data, chr = 'chr2', start = 171573000, end = 171574000)
 #'
+#' #without binding lines and smoothing
+#' draw.methylation(data, chr = 'chr2', start = 171573000, end = 171574000,
+#' bind.probes = F, smooth.methylation = F)
+#'
 #' # with changing some ggplot2 arguments:
 #' draw.methylation(data, chr = 'chr2', start = 171573000, end = 171574000, legend.title = 20,
-#' legend.position = 'bottom', plot.title = 28)
+#' legend.position = 'bottom', plot.title = 28, size.x.dot = 10, size.y.dot = 4)
 
 
-draw.methylation <- function(data, chromosom, start, end, plot.title = 26, axis.title.x  = 23,
+draw.methylation <- function(data, chromosom, start, end, bind.probes = T,
+                             smooth.methylation = T,
+                             size.x.dot = 15, size.y.dot = 9,
+                             plot.title = 26, axis.title.x  = 23,
                              axis.title.y = 23, legend.position = 'right', axis.text.x = 20, axis.text.y = 20,
                              legend.text = 16, legend.title = 18){
 
@@ -53,6 +62,11 @@ draw.methylation <- function(data, chromosom, start, end, plot.title = 26, axis.
       !all(data$meth + data$unmeth == data$no) | !all(round(data$meth /data$no,3) == round(data$meth.rate,3)))
     stop("Error: Incorrect values in data")
 
+  if(!is.logical(bind.probes))
+    stop("Error: Incorrect bind.probes argument")
+
+  if(!is.logical(smooth.methylation))
+    stop("Error: Incorrect smooth.methylation argument")
 
   DT <- data %>% filter(chr == chromosom, poz >=start, poz <= end)
 
@@ -62,18 +76,19 @@ draw.methylation <- function(data, chromosom, start, end, plot.title = 26, axis.
   DT  %>% dplyr::select(poz, chr, meth.rate, prob) %>% spread(prob, meth.rate) %>%
     rename(met_I = x, met_II = y) %>% left_join(DT1, by = c("poz", "chr")) -> DT
 
-  p <- ggplot(DT)  + xlim(start,end)  +
-    geom_point(DT,map=aes(x=poz, y=met_I, colour=cov_I), size = 15, shape = 20) +
-    scale_color_gradient(low="pink1", high="red", name = "log(covI)") +
-    geom_point(DT,map=aes(x=poz, y=met_II, fill=cov_II), size = 9, shape = 21) +
-    scale_fill_gradient(low="cadetblue1", high="royalblue4", name = "log(covII)")
+  p <- ggplot(DT) + ylim(0,1) +
+    scale_x_continuous(breaks = c(start, end),labels = c(start, end), limits = c(start, end)) +
+    geom_point(DT,map=aes(x=poz, y=met_I, colour=cov_I), size = size.x.dot , shape = 20) +
+    scale_color_gradient(low="pink1", high="red", name = "log(covX)") +
+    geom_point(DT,map=aes(x=poz, y=met_II, fill=cov_II), size = size.y.dot, shape = 21) +
+    scale_fill_gradient(low="cadetblue1", high="royalblue4", name = "log(covY)")
 
   x = as.matrix(DT[,1])
   y1 = as.matrix(DT[,'met_I'])
   y2 = as.matrix(DT[,'met_II'])
 
-  p <- p + geom_segment(aes(x = x, y = y1, xend = x, yend = y2) )  +
-    ggtitle(paste0("Methylation rate within two probes in ", chromosom)) +
+
+ p <- p + ggtitle(paste0("Methylation rate in ", chromosom)) +
     xlab("DNA position") + ylab("Methylation rate") + theme_minimal() +
     theme(
       plot.title = element_text(size=plot.title),
@@ -85,7 +100,18 @@ draw.methylation <- function(data, chromosom, start, end, plot.title = 26, axis.
       legend.text = element_text(size = legend.text),
       legend.title = element_text(size = legend.title),
       plot.margin = unit(c(1,1,1,1), "cm"))
-  p
 
+
+  if (bind.probes){
+    p <- p + geom_segment(aes(x = x, y = y1, xend = x, yend = y2) )
+  }
+
+  if (smooth.methylation){
+    p <-  p + geom_smooth(aes(x = poz,y = met_I), colour = 'red', se = FALSE)  +
+      geom_smooth(aes(x = poz,y = met_II), colour = "royalblue2", se = FALSE)
+  }
+
+  p
 }
+
 
